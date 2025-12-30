d693d4f28d904e1627bc1e5cd66358509a1fd1b0
Youssef Benihoud <youssef@ybenihoud.com>
2025-12-30 15:32:13 +0100

    add other mocking tests implementations

--- tests/mocking.test.js
+++ tests/mocking.test.js
@@ -1,8 +1,10 @@
-import { vi, it, expect, describe } from "vitest";
+import { vi, it, expect, describe, beforeEach } from "vitest";
 import { getShippingQuote } from "../src/libs/shipping";
-import { getShippingInfo } from "../src/mocking";
+import { getShippingInfo, submitOrder } from "../src/mocking";
+import { charge } from "../src/libs/payment";
 
 vi.mock("../src/libs/shipping");
+vi.mock("../src/libs/payment");
 
 describe("test suite", () => {
   it("test case", () => {
@@ -39,5 +41,39 @@ describe("getShippingInfo", () => {
     const result = getShippingInfo("Nowhere");
 
     expect(result).toMatch(/unavailable/i);
+  });
+});
+
+describe("submitOrder", () => {
+  let order;
+  let creditCard;
+
+  beforeEach(() => {
+    order = { totalAmount: 100 };
+    creditCard = { creditCardNumber: "1".repeat };
+  });
+
+  it("should charge the customer", async () => {
+    vi.mocked(charge).mockResolvedValue({ success: true });
+
+    await submitOrder(order, creditCard);
+
+    expect(charge).toHaveBeenCalledWith(creditCard, order.totalAmount);
+  });
+
+  it("the charge should be successfull", async () => {
+    vi.mocked(charge).mockResolvedValue({ success: true });
+
+    const result = await submitOrder(order, creditCard);
+
+    expect(result).toEqual({ success: true });
+  });
+
+  it("the charge should be failed", async () => {
+    vi.mocked(charge).mockResolvedValue({ status: "failed" });
+
+    const result = await submitOrder(order, creditCard);
+
+    expect(result).toEqual({ success: false, error: "payment_error" });
   });
 });
